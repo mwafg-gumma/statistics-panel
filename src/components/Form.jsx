b90468fa1fc72c2c14253f502ab08152b3d47401
mwafg gumma <mwafggommaabdula@gmail.com>
2025-07-14 10:59:08 +0200

    fix image path

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import Back from '../assets/image/Back.png'
+// import Back from '../assets/image/Back.png'
 import {FaUser,FaRoute} from 'react-icons/fa'
 import { NavLink } from 'react-router-dom'
 
@@ -71,7 +71,7 @@ return (
             <div className="mx-3 relative flex items-center justify-center">
             <img
                 // src="/assets/image/back.png"
-                src={Back}
+                // src={Back}
                 alt="Sing Img"
                 className="rounded-md w-[700px] h-[450px] object-cover brightness-75"
                 style={{ backgroundColor: "white" }}
